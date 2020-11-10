ec43e30bc739ff6128b0f19608b6ad42ca391bfb
Anthony Perozich <avp102@francis.edu>
2020-11-10 00:36:28 -0500

    Was able to get the correct number of logs in the inventory to display.

--- src/main/java/com/LogsInInventory/LogsInInventoryOverlay.java
+++ src/main/java/com/LogsInInventory/LogsInInventoryOverlay.java
@@ -1,8 +1,10 @@
 package com.LogsInInventory;
 
 
-import com.jogamp.nativewindow.util.Dimension;
 import net.runelite.api.Client;
+import net.runelite.api.InventoryID;
+import net.runelite.api.Item;
+import net.runelite.api.ItemContainer;
 import net.runelite.client.ui.overlay.Overlay;
 import net.runelite.client.ui.overlay.OverlayPosition;
 import net.runelite.client.ui.overlay.components.LineComponent;
@@ -12,19 +14,21 @@ import net.runelite.client.ui.overlay.components.TitleComponent;
 import javax.inject.Inject;
 import java.awt.*;
 
+
 public class LogsInInventoryOverlay extends Overlay {
 
     private final Client client;
     private final PanelComponent panelComponent = new PanelComponent();
 
     @Inject
-    private LogsInInventoryOverlay(Client client){
+    private LogsInInventoryOverlay(Client client) {
         setPosition(OverlayPosition.ABOVE_CHATBOX_RIGHT);
         this.client = client;
     }
 
+
     @Override
-    public java.awt.Dimension render(Graphics2D graphics){
+    public java.awt.Dimension render(Graphics2D graphics) {
         panelComponent.getChildren().clear();
         String overlayTitle = "Logs In Inventory";
 
@@ -32,9 +36,24 @@ public class LogsInInventoryOverlay extends Overlay {
 
         panelComponent.setPreferredSize(new java.awt.Dimension(graphics.getFontMetrics().stringWidth(overlayTitle) + 30, 0));
 
-        panelComponent.getChildren().add(LineComponent.builder().right(Integer.toString(client.getWorld())).build());
+        panelComponent.getChildren().add(LineComponent.builder().right(Integer.toString(getLogs())).build());
 
         return panelComponent.render(graphics);
 
+    }
+
+    public int getLogs() {
+        final ItemContainer itemContainer = client.getItemContainer(InventoryID.INVENTORY);
+        final Item[] items = itemContainer.getItems();
+        int counter = 0;
+
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null) {
+                if (items[i].getId() == 1511) {
+                    counter++;
+                }
+            }
+        }
+        return counter;
     }
 }
